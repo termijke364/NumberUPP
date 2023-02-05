fbeff4d9b7aa54dcade5345425502cb08f9dc218
Nikita Pepelyaev <termi364@mail.ru>
2023-02-05 15:06:31 +0400

    Add project files.

---
+++ NumberUPP/NumberUPP.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double Recurs(int i, double x)
+{
+	if (i == 1)
+		return x;
+
+	else if (i > 0)
+		return x * Recurs(--i, x);
+
+	else if (i < 0)
+		return 1 / Recurs(-i, x);
+}
+
+int main()
+{
+	setlocale(0, "");
+
+	double number;
+	int up;
+
+	cout << "Вычисление a^x" << endl;
+
+	cout << "Введите число а:" << endl;
+	cin >> number;
+
+	cout << "Введите степень числа а" << endl;
+	cin >> up;
+
+	cout << "a ^ x = " << Recurs(up, number) << endl;
+
+	return 0;
+}
